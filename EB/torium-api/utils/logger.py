965caeb6f86a89e5c49b4a764812ec36975474ba
aciesielczyk <aciesielczyk@novelaneuro.com>
2022-09-25 19:07:39 +0200

    logger fix

--- EB/torium-api/utils/logger.py
+++ EB/torium-api/utils/logger.py
@@ -1,5 +1,4 @@
 from functools import wraps
-import inspect
 
 
 def log(func):
@@ -8,19 +7,15 @@ def log(func):
         from application import logger
         class_name = type(args[0]).__name__
         function_name = func.__name__
-        method_attributes = inspect.getfullargspec(func)[0]
-        message = construct_logger(class_name, function_name, method_attributes, args)
+        message = construct_logger(class_name, function_name, kwargs)
         logger.info(message)
         result = func(args[0], args[1], **kwargs)
         return result
     return wrapper
 
 
-def construct_logger(class_name, function_name, method_attributes, args):
-    base = f"{class_name}.{function_name}(): "
-    if args:
-        return base + ' '.join([f"{method_attributes[i]}: {args[i]}," for i in range(1, len(method_attributes))])
-    return base
+def construct_logger(class_name, function_name, kwargs):
+    return f"{class_name}.{function_name}(): {kwargs}"
 
 
 def log_exception(class_name: str, error: str):
